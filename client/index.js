edebf2ef531738ab441586f9ea35703fa81b1a59
Felix Faerber <ffaerber@gmail.com>
2017-10-05 19:17:20 +0000

    status

--- client/index.js
+++ client/index.js
@@ -1,23 +1,29 @@
 require('dotenv').config()
+const mqtt = require('mqtt')
+const messageBus = mqtt.connect(process.env.MESSAGE_BUS_HOST)
+const groundStation = require('socket.io-client')(process.env.GROUND_STATION_HOST);
 
-var gpsd = require('node-gpsd');
-var mqtt = require('mqtt')
-
-console.log('connect to ' + process.env.MESSAGE_BUS_HOST)
-var mqttClient = mqtt.connect(process.env.MESSAGE_BUS_HOST)
-
-mqttClient.on('connect', () => {
-  console.log('socketio connected');
-  daemon.start(() => {
-    console.log('gpsd started');
-    var listener = new gpsd.Listener();
-
-    listener.on('TPV', (tpv) => {
-      mqttClient.publish('gps', JSON.stringify(tpv))
-    });
-
-    listener.connect(() => {
-      listener.watch();
-    });
-  });
+messageBus.on('connect', () => {
+  console.log('subscripe to gps messageBus channel')
+  messageBus.subscribe('gps')
 })
+
+messageBus.on('message', (topic, message) => {
+  //console.log('new messageBus message in channel ' + topic + ' with message ' + message)
+  groundStation.emit(message)
+})
+
+console.log('connecting to groundStaion ' + process.env.GROUND_STATION_HOST)
+groundStation.on('connect', () => {
+  console.log('groundStation connected')
+});
+
+groundStation.on('event', (data) => {
+  groundStation.emit(data)
+  console.log('groundStation event' + data)
+});
+
+groundStation.on('disconnect', () => {
+  console.log('groundStation disconnected')
+});
+
