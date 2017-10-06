9e8326fb2a771a0d89b48b72ccf73ede0a0f26ff
Felix Faerber <ffaerber@gmail.com>
2017-10-06 12:35:48 +0000

    remove picam

--- camera/index.js
+++ camera/index.js
@@ -1,33 +1,13 @@
 require('dotenv').config()
+var RaspiCam = require("raspicam");
 
-var camera = new RaspiCam({
-    mode: 'video',
-    output: '-',
-    w: 180
-});
+//var camera = new RaspiCam({
+//  mode: 'photo',
+//  output: '-'
+//});
 
 //to take a snapshot, start a timelapse or video recording
-camera.start( );
+//camera.start( );
 
-//to stop a timelapse or video recording
-camera.stop( );
 
-//listen for the "start" event triggered when the start method has been successfully initiated
-camera.on("start", function(){
-	//do stuff
-});
 
-//listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, timestamp, filename){
-	//do stuff
-});
-
-//listen for the "stop" event triggered when the stop method was called
-camera.on("stop", function(){
-	//do stuff
-});
-
-//listen for the process to exit when the timeout has been reached
-camera.on("exit", function(){
-	//do stuff
-});
