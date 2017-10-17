e69da797f4bb1ea63ceab1cfc9162a6b3d800c86
Felix Faerber <ffaerber@gmail.com>
2017-10-17 17:12:17 +0200

    thunderborg i2c connect

--- thunderborg/Main.py
+++ thunderborg/Main.py
@@ -1,6 +1,32 @@
+#!/usr/bin/env python
+# coding: Latin-1
+
+# Import library functions we need
 import os
 import paho.mqtt.client as mqtt
 import json
+import ThunderBorg
+import time
+import sys
+
+# Setup the ThunderBorg
+TB = ThunderBorg.ThunderBorg()     # Create a new ThunderBorg object
+#TB.i2cAddress = 0x15              # Uncomment and change the value if you have changed the board address
+TB.Init()                          # Set the board up (checks the board is connected)
+if not TB.foundChip:
+    boards = ThunderBorg.ScanForThunderBorg()
+    if len(boards) == 0:
+        print 'No ThunderBorg found, check you are attached :)'
+    else:
+        print 'No ThunderBorg at address %02X, but we did find boards:' % (TB.i2cAddress)
+        for board in boards:
+            print '    %02X (%d)' % (board, board)
+        print 'If you need to change the I�C address change the setup line so it is correct, e.g.'
+        print 'TB.i2cAddress = 0x%02X' % (boards[0])
+    sys.exit()
+
+# Disable the colour by battery level
+TB.SetLedShowBattery(False)
 
 print('connecting to ' + os.environ['MESSAGE_BUS_HOST'])
 
